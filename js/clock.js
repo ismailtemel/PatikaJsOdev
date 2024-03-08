686e405dc6a985417a1694ddf13cda93302a56ae
ismailtemel <64991440+ismailtemel@users.noreply.github.com>
2024-03-08 11:34:36 +0300

    Update

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+let name = prompt("Lütfen adınızı giriniz...");
+const days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+
+setInterval(function(){
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    let hourShow = hour.toString().length == 1 ? "0" + hour.toString() : hour.toString();
+    let minuteShow = minute.toString().length == 1 ? "0" + minute.toString() : minute.toString();
+    let secondShow = second.toString().length == 1 ? "0" + second.toString() : second.toString();
+    let dayShow = days[date.getDay()];
+
+    let timeDay = `${hourShow}:${minuteShow}:${secondShow}:${dayShow}`;
+    clockDom.innerText = `${timeDay}`;
+    },1000);
+
+    const nameDOM = document.getElementById("myName");
+    const clockDOM = document.getElementById("myClock");
+
+    nameDOM.innerText = `${name}`;
